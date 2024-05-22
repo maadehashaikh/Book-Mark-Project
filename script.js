b9d260590f4d85722bc95f6c4d9bede20ed83ab2
Maadeha shaikh <maadeham@gmail.com>
2024-05-22 09:10:45 +0500

    new commit

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-let bookMark_Data = document.getElementById("bookmark");
-console.log(bookMark_Data);
+const addBookMark = () => {
+  let bookMark_Data = document.getElementById("bookmark").value;
+  let div = document.getElementById("bookMark");
+  let li = document.createElement("li");
+  li.innerText = bookMark_Data;
+  div.appendChild(li);
+  bookMark_Data.innerText = "";
+}
+
+
+
